0873f5f0e29eca9fb4e6f9f332985354a3cb2e3d
Saipavan1612 <saipavangurram@gmail.com>
2024-09-18 15:26:03 +0530

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,17 @@ import { LearningResourcesComponent } from './learning-resources/learning-resour
 import { AuthService } from './auth/auth.service';
 import { NgIf } from '@angular/common';
 import { AuthDirective } from './auth/auth.directive';
+import { LogDirective } from './log.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective],
+  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective, LogDirective],
 })
 export class AppComponent {
-    private authService =  inject(AuthService);
+  private authService = inject(AuthService);
 
-    isAdmin = computed(() => this.authService.activePermission() === 'admin');
+  isAdmin = computed(() => this.authService.activePermission() === 'admin');
 }
