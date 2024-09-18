128e0f21143e8e32ceb93c64e6076fcf47b844be
Saipavan1612 <saipavangurram@gmail.com>
2024-09-18 13:47:44 +0530

    auth directive

---
+++ src/app/auth/auth.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, inject, input } from '@angular/core';
+import { Permission } from './auth.model';
+import { AuthService } from './auth.service';
+
+@Directive({
+  selector: '[appAuth]',
+  standalone: true
+})
+export class AuthDirective {
+
+  userType = input.required<Permission>();
+  private authService = inject(AuthService);
+  constructor() { }
+
+}
