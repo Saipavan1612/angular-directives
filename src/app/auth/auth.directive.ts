ddb93c7acc4d5e9445950cb067145951c127296f
Saipavan1612 <saipavangurram@gmail.com>
2024-09-18 14:55:57 +0530

    structure directive

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, inject, input } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, effect, inject, input } from '@angular/core';
 import { Permission } from './auth.model';
 import { AuthService } from './auth.service';
 
@@ -8,8 +8,19 @@ import { AuthService } from './auth.service';
 })
 export class AuthDirective {
 
-  userType = input.required<Permission>();
+  userType = input.required<Permission>({ alias: 'appAuth' });
   private authService = inject(AuthService);
-  constructor() { }
+  private templateRef = inject(TemplateRef)
+  private viewContainerRef = inject(ViewContainerRef);
+
+  constructor() {
+    effect(() => {
+      if (this.authService.activePermission() === this.userType()) {
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
+      } else {
+        this.viewContainerRef.clear();
+      }
+    })
+  }
 
 }
